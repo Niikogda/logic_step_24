e66a8de45d2f880d7b17104f24b0c1a1f7e4820b
Laptop <Laptop@DESKTOP-BVI07RL>
2023-03-25 15:06:11 +0200

    done_2

--- logic_step.py
+++ logic_step.py
@@ -1,1 +1,27 @@
 
+x=int(input("enter first number: "))
+y=int(input("enter second number: "))
+z=int(input("enter third number: "))
+a=input("choose your action: max, min or average: ")
+if a == ("max"):
+    if x>z and x>y:
+        print(int(x))
+    elif y>x and y>z:
+        print(int(y))
+    elif z>y and z>x:
+        print(int(z))
+    else: print("mistake in the first task")
+elif a == ("min"):
+    if x<y and x<z:
+        print(int(x))
+    elif y<x and y<z:
+        print(int(y))
+    elif z<y and z<x:
+        print(int(z))
+    else:
+        print("mistake in the second task")
+elif a == ("average"):
+    print(int((x+y+z)/3))
+else:
+    print("i don`t understand you, sorry;))")
+
