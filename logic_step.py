d0532efde4e012d0f158e7cad7a1af39e5c3d119
Laptop <Laptop@DESKTOP-BVI07RL>
2023-03-25 15:17:50 +0200

    done_3

--- logic_step.py
+++ logic_step.py
@@ -1,1 +1,11 @@
 
+x=float(input("enter amount of meters: "))
+y= input("Convert to (miles, inches, yards): ")
+if y==("miles"):
+    print(float(x/1609.344))
+elif y==("inches"):
+    print(float(x*39.37))
+elif y==("yards"):
+    print(float(x*1.0936))
+else:
+    print("mistake")
