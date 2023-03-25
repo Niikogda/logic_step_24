be407354d6795de596e4cff8c0539629deea89fb
Laptop <Laptop@DESKTOP-BVI07RL>
2023-03-25 14:06:24 +0200

    done_5

--- logic_step.py
+++ logic_step.py
@@ -1,1 +1,14 @@
 
+x=int(input("enter first number: "))
+z=input("enter an action: ")
+y=int(input("enter second number: "))
+if z == ("+"):
+    print(int(x+y))
+elif z== ("-"):
+    print(int(x-y))
+elif z == ("*"):
+    print(int(x*y))
+elif z == ("/"):
+    print(int(x/y))
+else:
+    print("i don`t understand you, sorry")
