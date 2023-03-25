be77cf68497c40bb52607470696c4130b14af3a8
Laptop <Laptop@DESKTOP-BVI07RL>
2023-03-25 14:22:51 +0200

    done_1

--- logic_step.py
+++ logic_step.py
@@ -1,1 +1,11 @@
 
+x=int(input("enter first number: "))
+y=int(input("enter second number: "))
+z=int(input("enter third number: "))
+a=input("choose sum or prod: ")
+if a == ("sum"):
+    print(int(x+y+z))
+elif a == ("prod"):
+    print(int(x*y*z))
+else:
+    print("you made a mistake, sorry;)")
